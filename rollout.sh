3a5450b6e6c81242e6bc5016b62aa752270fe8ea
Nick Marasco <nicholas.marasco@ibm.com>
2018-03-02 10:59:56 -0500

    Script will pass in working commit log entry on failed pull

--- rollout.sh
+++ rollout.sh
@@ -3,6 +3,7 @@
 msg_headers=("MONITOR" "UPDATE" "ERROR")
 retry_count=0
 return_code=0
+bot_status=""
 
 # Messaging function
 function post_msg {
@@ -14,7 +15,7 @@ function post_msg {
 function start_bot {
   retry_count=0
   while [ $retry_count -lt 5 ]; do
-    python pylon_factory.py & pylon_pid=$!
+    python pylon_factory.py "$bot_status" & pylon_pid=$!
     sleep 2
     if kill -0 "$pylon_pid"; then
       break
@@ -24,6 +25,7 @@ function start_bot {
   done
   if [ $retry_count -ge 5 ]; then
     post_msg ${msg_headers[2]} "Process failed to start, possibly broken build"
+    #post_msg ${msg_headers[1]} "Reverting to previous commit"
     exit 1
   fi
 }
@@ -43,6 +45,8 @@ while sleep 1; do
     # RC=0 , clean exit
     if [ $pylon_exit -eq 0 ]; then
       post_msg ${msg_headers[0]} "pylon_factory.py exited with RC=$pylon_exit"
+      post_msg ${msg_headers[0]} "Script terminating"
+      exit 0
 
     # RC=1 , clean exit and update
     elif [ $pylon_exit -eq 1 ]; then
@@ -52,12 +56,15 @@ while sleep 1; do
 
       if [ $return_code -eq 0 ]; then
         post_msg ${msg_headers[1]} "Pull successful, restarting bot with new code"
+        bot_status=""
       else
         post_msg ${msg_headers[2]} "Pull unsuccessful, restarting bot with old code"
+        bot_status="`git log --name-status HEAD^..HEAD`"
       fi
 
     # RC=? , unknown return code from bot, attempt to restart
     else
       post_msg ${msg_headers[2]} "Unrecognized return code, attempting restart"
+      bot_status=""
     fi
 done
